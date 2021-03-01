44a30f3a9570253615021c9b77d9b24bc76f38c8
Ally <el.ally741@gmail.com>
2021-03-01 16:40:27 +0200

    heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,16 +4,28 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
-    ssl: false,
+    dialectOptions: {
+      ssl: {
+        require: true, // This will help you. But you will see nwe error
+        rejectUnauthorized: false // This line will fix new error
+      }
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
-    ssl: false,
+      dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    ssl: false,
+      dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
   },
 };
