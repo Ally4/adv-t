6e39aeef34dcf84c56e38e783416f2751886f762
Ally <el.ally741@gmail.com>
2021-03-01 16:34:51 +0200

    heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,16 +4,16 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: 'postgres',
-    ssl: true,
+    ssl: false,
   },
   test: {
     url: process.env.TEST_DATABASE_URL,
     dialect: 'postgres',
-    ssl: true,
+    ssl: false,
   },
   production: {
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    ssl: true,
+    ssl: false,
   },
 };
